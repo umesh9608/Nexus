f3510c6de0774c4fe6649911b7e2c72b5695c506
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-06-02 10:54:01 +0530

    React Router

---
+++ React/Day_12/src/Contact.js
@@ -0,0 +1,9 @@
+
+export default function Contact(){
+
+    return(
+        <>
+        <h1>Welcome to  Contact</h1>
+        </>
+    )
+}
---
+++ React/Day_12/src/Hello.js
@@ -0,0 +1,9 @@
+
+export default function Hello(){
+
+    return(
+        <>
+        <h1>Welcome to  Details/hello page</h1>
+        </>
+    )
+}
